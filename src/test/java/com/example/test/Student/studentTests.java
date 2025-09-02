ba6377021fd738c7241463cb8e37edcc47698ea5
Naqda Global <naqdaglobal@gmail.com>
2025-09-02 12:08:46 +0430

    change docker file

--- src/test/java/com/example/test/Student/studentTests.java
+++ src/test/java/com/example/test/Student/studentTests.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
@@ -47,6 +49,13 @@ public class studentTests {
             .withDatabaseName("testdb")
             .withUsername("postgres")
             .withPassword("786@admin");
+    
+    @DynamicPropertySource
+    static void overrideDatasourceProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
+    }
 
     @Test
     void testDatabaseConnection() {
